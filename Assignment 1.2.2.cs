67db7fbefcd4c503948c7c7ff074b27409d0b637
Chiblood <chi.blood6@gmail.com>
2025-08-26 11:56:48 -0400

    Added a solution

--- Assignment 1.2.2.cs
+++ Assignment 1.2.2.cs
@@ -1,9 +1,17 @@
-﻿using System.Linq;
-/* Write a C# Sharp program to find the sum of first 10 natural numbers.
+﻿/* Write a C# Sharp program to find the sum of first 10 natural numbers.
 Expected Output :
 The first 10 natural number is :
 1 2 3 4 5 6 7 8 9 10
 The Sum is : 55
 */
-int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-Console.WriteLine("The Sum is : " + myArray.Sum());
+// Cheat solution
+// using System.Linq;
+// int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+// Console.WriteLine("The Sum is : " + myArray.Sum());
+
+// Actual solution
+int sum = 0;
+for (int i = 1; i <= 10; i++) {
+    sum += i;
+}
+Console.WriteLine("The Sum is : " + sum);
